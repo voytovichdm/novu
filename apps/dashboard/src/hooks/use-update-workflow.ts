96c2829941a49a3f447b786b975834329dc697fd
Paweł Tymczuk <LetItRock@users.noreply.github.com>
2024-11-18 14:54:06 +0100

    fix(dashboard): update the workflow query data when its updated (#7051)

--- apps/dashboard/src/hooks/use-update-workflow.ts
+++ apps/dashboard/src/hooks/use-update-workflow.ts
@@ -1,13 +1,22 @@
 import { updateWorkflow } from '@/api/workflows';
+import { useEnvironment } from '@/context/environment/hooks';
+import { QueryKeys } from '@/utils/query-keys';
 import type { WorkflowResponseDto } from '@novu/shared';
-import { useMutation, UseMutationOptions } from '@tanstack/react-query';
+import { useMutation, UseMutationOptions, useQueryClient } from '@tanstack/react-query';
 
 export const useUpdateWorkflow = (
   options?: UseMutationOptions<WorkflowResponseDto, unknown, Parameters<typeof updateWorkflow>[0]>
 ) => {
+  const { currentEnvironment } = useEnvironment();
+  const queryClient = useQueryClient();
+
   const mutation = useMutation({
     mutationFn: updateWorkflow,
     ...options,
+    onSuccess: async (data, variables, context) => {
+      await queryClient.setQueryData([QueryKeys.fetchWorkflow, currentEnvironment?._id, data.slug], data);
+      options?.onSuccess?.(data, variables, context);
+    },
   });
 
   return {
