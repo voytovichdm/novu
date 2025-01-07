e9571c3aff82a5da6521a7a1671f2fcb4f63172e
Paweł Tymczuk <LetItRock@users.noreply.github.com>
2025-01-07 10:21:05 +0100

    fix(api-service): on global preferences update reset workflow preferences per env (#7428)
    Co-authored-by: Sokratis Vidros <SokratisVidros@users.noreply.github.com>

--- apps/api/src/app/subscribers/e2e/update-global-preference.e2e.ts
+++ apps/api/src/app/subscribers/e2e/update-global-preference.e2e.ts
@@ -1,14 +1,17 @@
-import { ChannelTypeEnum } from '@novu/shared';
+import { ChannelTypeEnum, StepTypeEnum } from '@novu/shared';
 import { UserSession } from '@novu/testing';
 import { expect } from 'chai';
 
-import { updateGlobalPreferences } from './helpers';
+import { SubscriberRepository } from '@novu/dal';
+import { getPreference, updateGlobalPreferences } from './helpers';
 
 describe('Update Subscribers global preferences - /subscribers/:subscriberId/preferences (PATCH)', function () {
   let session: UserSession;
+  let subscriberRepository: SubscriberRepository;
 
   beforeEach(async () => {
     session = new UserSession();
+    subscriberRepository = new SubscriberRepository();
     await session.initialize();
   });
 
@@ -87,6 +90,69 @@ describe('Update Subscribers global preferences - /subscribers/:subscriberId/pre
       [ChannelTypeEnum.SMS]: true,
       [ChannelTypeEnum.IN_APP]: false,
     });
+  });
+
+  it('should update user global preferences only for the current environment', async function () {
+    // create a template in dev environment
+    await session.createTemplate({
+      steps: [
+        {
+          type: StepTypeEnum.IN_APP,
+          content: 'Hello',
+        },
+      ],
+      noFeedId: true,
+    });
+
+    await session.switchToProdEnvironment();
+    // create a subscriber in prod environment
+    await subscriberRepository.create({
+      _environmentId: session.environment._id,
+      _organizationId: session.organization._id,
+      subscriberId: session.subscriberId,
+    });
+    // create a template in prod environment
+    await session.createTemplate({
+      steps: [
+        {
+          type: StepTypeEnum.IN_APP,
+          content: 'Hello',
+        },
+      ],
+      noFeedId: true,
+    });
+
+    await session.switchToDevEnvironment();
+    // update the subscriber global preferences in dev environment
+    const response = await updateGlobalPreferences(
+      {
+        enabled: true,
+        preferences: [{ type: ChannelTypeEnum.IN_APP, enabled: false }],
+      },
+      session
+    );
+
+    expect(response.data.data.preference.enabled).to.eql(true);
+    expect(response.data.data.preference.channels).to.eql({
+      [ChannelTypeEnum.EMAIL]: true,
+      [ChannelTypeEnum.PUSH]: true,
+      [ChannelTypeEnum.CHAT]: true,
+      [ChannelTypeEnum.SMS]: true,
+      [ChannelTypeEnum.IN_APP]: false,
+    });
+
+    // get the subscriber preferences in dev environment
+    const getDevPreferencesResponse = await getPreference(session);
+    const devPreferences = getDevPreferencesResponse.data.data;
+    expect(devPreferences.every((item) => !!item.preference.channels.in_app)).to.be.false;
+
+    await session.switchToProdEnvironment();
+
+    // get the subscriber preferences in prod environment
+    session.apiKey = session.environment.apiKeys[0].key;
+    const getProdPreferencesResponse = await getPreference(session);
+    const prodPreferences = getProdPreferencesResponse.data.data;
+    expect(prodPreferences.every((item) => !!item.preference.channels.in_app)).to.be.true;
   });
 
   // `enabled` flag is not used anymore. The presence of a preference object means that the subscriber has enabled notifications.
